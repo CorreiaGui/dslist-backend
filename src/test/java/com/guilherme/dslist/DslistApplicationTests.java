a35924a9ff47184b62d455b0c9b7ba5ae3683d7f
Guilherme Correia <guilherme.martins@contmatic.com.br>
2023-07-11 09:31:02 -0300

    add projeto

---
+++ src/test/java/com/guilherme/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
